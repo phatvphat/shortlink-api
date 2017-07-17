9b7087e4bb00214fd0be940ed27a466979b010d4
PvP Phát Nguyễn <phatvalong@gmail.com>
2017-07-18 01:27:29 +0700

    Update random.php

--- random.php
+++ random.php
@@ -9,17 +9,20 @@ include 'func/shortlink.php';
 
 $link = 'https://www.facebook.com/pvp.phatnguyen'; // Link cần rút gọn
 
-$a = array('adf','shorte');
-$random_key = array_rand($a); // Lấy ngẫu nhiên số và so sánh // Tỉ lệ trùng lần nữa 70% :v
+function shortlink($link){
+	$a = array('adf','shorte');
+	$random_key = array_rand($a); // Lấy ngẫu nhiên số và so sánh // Tỉ lệ trùng lần nữa 70% :v
 
-switch($a[$random_key]){ // so sánh xem nó cho ra thằng nào thì rút gọn bằng thằng đó...
-	case 'adf':
-		$short_link = get_adf($link);
-	break;
-	case 'shorte':
-		$short_link = get_shorte($link);
-	break;
+	switch($a[$random_key]){ // so sánh xem nó cho ra thằng nào thì rút gọn bằng thằng đó...
+		case 'adf':
+			$short_link = get_adf($link);
+		break;
+		case 'shorte':
+			$short_link = get_shorte($link);
+		break;
+	}
+return $short_link; // Hiển thị link ngẫu nhiên // P/s: Cho ra link nào nhở... :))))
 }
 
-echo $short_link; // Hiển thị link ngẫu nhiên // P/s: Cho ra link nào thế nhở... :))))
+echo shortlink($link); // Mẫu thử
 ?>
